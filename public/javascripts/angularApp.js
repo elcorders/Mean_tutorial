a5f127da08d1de253578720b503e786c77a39757
ELcoders <dnd@elcorders.me>
2015-11-26 00:06:23 +0530

    Revert "Revert "Node Js ReConfig""
    This reverts commit ff142369b2dd728418e50770d0b9fdc616f75bc0.

---
+++ public/javascripts/angularApp.js
@@ -0,0 +1,95 @@
+﻿
+var app = angular.module('flapperNews', ['ui.router']);
+app
+//service
+.factory('posts', [function () {
+        var o = {
+            posts: [
+                {
+                    title: "hello",
+                    link: '',
+                    upvotes: 0,
+                    comments: [
+                        { author: 'Joe', body: 'Cool Post #YOLO', upvotes: 0 },
+                        { author: 'Bob', body: 'lorema ispum do sit amet #META', upvotes: 0 }
+                    ]
+                }
+            ]
+        };
+        return o;
+    }])
+
+.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider , $urlRouterProvider) {
+
+        $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: '/home.html',
+            controller: 'MainCtrl'
+        })
+        .state('posts', {
+            url: '/posts/{id}', // route parameter
+            templateUrl: '/posts.html',
+            controller: 'PostsCtrl'
+        });
+
+        $urlRouterProvider.otherwise('home');
+    }
+])
+
+//home controller
+.controller('MainCtrl', [
+    '$scope',
+    'posts',
+    function ($scope, posts){
+        //$scope.test = 'Hello world!';        
+        $scope.posts = posts.posts;
+        
+        $scope.addPost = function () {
+            if (!$scope.title || $scope.title === "") { return;}
+            $scope.posts.push({
+                title: $scope.title,
+                link: $scope.link,
+                upvotes: 0,
+                comments: [
+                    { author: 'Joe', body: 'Cool Post #YOLO', upvotes: 0 },
+                    {author: 'Bob', body: 'lorema ispum do sit amet #META', upvotes: 0 }
+                ]
+            });
+            $scope.title = '';
+            $scope.link = '';
+        };
+        $scope.incrementUpvotes = function(post) {
+            post.upvotes += 1;
+        }
+    }
+])
+
+//posts controller
+.controller('PostsCtrl', [
+    '$scope',
+    '$stateParams',
+    'posts',
+    function ($scope, $stateParams, posts){
+
+        $scope.post = posts.posts[$stateParams.id];
+
+        $scope.addComment = function () {
+            console.log("fired");
+            if (!$scope.body === "") { return; }
+            $scope.post.comments.push({
+                body: $scope.body,
+                author: 'user',
+                upvotes: 0
+            });
+            $scope.body = '';
+        };
+
+        $scope.incrementUpvotes = function (comment) {
+           comment.upvotes += 1;
+        }
+    }
+])

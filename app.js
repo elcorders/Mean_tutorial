581e69d9aec7d413344e4078e18c0a0f70889e1c
ELcoders <dnd@elcorders.me>
2015-11-26 17:46:47 +0530

    Set Up Passport

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 var mongoose = require('mongoose');
+var passport = require('passport');
 
 require('./models/Posts');
 require('./models/Comments');
+
+require('./models/Users');
+
+require('./config/passport');
+
 mongoose.connect('mongodb://localhost/news');
 
 
@@ -12,11 +18,15 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+
+
 var app = express();
 
+app.use(passport.initialize());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
